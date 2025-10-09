0d0ee3e361ef298e2ef65877f1911f3a77f95cc7
tieumi tsiazoc leslye nivele <tieumitsiazocleslyenivele@gmail.com>
2025-10-09 07:20:26 +0100

    Update image.c

--- image.c
+++ image.c
@@ -145,29 +145,6 @@ void image_en_gris(Image *img) {
     }
 }
 
-/* ---------- Programme principal de test ---------- */
-int main() {
-    char entree[100], sortie[100];
-    printf("Nom du fichier PPM à convertir : ");
-    scanf("%s", entree);
-    printf("Nom du fichier de sortie : ");
-    scanf("%s", sortie);
-
-    Image *img = read_p3(entree);
-    if (!img) {
-        printf("Erreur de lecture de l'image.\n");
-        return 1;
-    }
-
-    image_en_gris(img);
-    write_p3(sortie, img);
-
-    printf("Image convertie en gris avec succès : %s\n", sortie);
-
-    free(img->data);
-    free(img);
-    return 0;
-}
 
 
 
